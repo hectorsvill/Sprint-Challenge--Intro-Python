253a845f30e9a031b508571aeb22dab112914f72
hectorsvill <hectorstevenvillasano@gmail.com>
2019-10-04 08:57:03 -0700

    age plus 10

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -24,13 +24,13 @@ humans = [
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with 'D':
 print("Starts with D:")
-a = []
+a = [h.name for h in humans if h.name[0] == "D"]
 print(a)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name ends in "e".
 print("Ends with e:")
-b = []
+b = [h.name for h in humans if h.name[-1] == "e"]
 print(b)
 
 # Write a list comprehension that creates a list of names of everyone
@@ -41,7 +41,7 @@ print(c)
 
 # Write a list comprehension that creates a list of all the ages plus 10.
 print("Ages plus 10:")
-d = []
+d = [h.age + 10 for h in humans]
 print(d)
 
 # Write a list comprehension that creates a list of strings which are the name
